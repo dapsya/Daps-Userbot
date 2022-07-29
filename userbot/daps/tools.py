faeceee02c69cb0124b5231a28aa96d3809634b3
dapsya <108969276+dapsya@users.noreply.github.com>
2022-07-30 02:44:46 +0700

    Update tools.py

--- userbot/daps/tools.py
+++ userbot/daps/tools.py
@@ -320,8 +320,8 @@ async def bash(cmd):
 
 def post_to_telegraph(title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "Ayiin-Userbot"
-    auth_url = "https://github.com/AyiinXd/Ayiin-Userbot"
+    auth_name = "dapsya-Userbot"
+    auth_url = "https://github.com/dapsya/Daps-Userbot"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=title,
