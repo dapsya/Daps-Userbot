cda448fa88357e60fb31f4c3ac437ec4450904d2
dapsya <108969276+dapsya@users.noreply.github.com>
2022-07-30 23:27:42 +0700

    Update converter.py

--- userbot/daps/converter/converter.py
+++ userbot/daps/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from AyiinXd.ayiin.exceptions import FFmpegReturnCodeError
+from userbot.ayiin.exceptions import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
