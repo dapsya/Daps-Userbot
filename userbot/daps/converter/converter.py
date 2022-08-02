a9f9f03615dfb01600175a28461d735694c62d13
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-08-03 01:40:23 +0700

    Update converter.py

--- userbot/daps/converter/converter.py
+++ userbot/daps/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from userbot.ayiin.exceptions import FFmpegReturnCodeError
+from userbot.daps.exceptions import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
